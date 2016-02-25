7195ab00c62f723eebceb0f911b83b58003c7e29
Wh <mysky0123@163.com>
2016-02-25 14:38:26 +0800

    init Functional-Module

---
+++ Functional-Module/Functional-Module/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Functional-Module
+//
+//  Created by admin on 16/2/25.
+//  Copyright (c) 2016年 吴河-admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Functional-Module/Functional-ModuleTests/Functional_ModuleTests.m
@@ -0,0 +1,40 @@
+//
+//  Functional_ModuleTests.m
+//  Functional-ModuleTests
+//
+//  Created by admin on 16/2/25.
+//  Copyright (c) 2016年 吴河-admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Functional_ModuleTests : XCTestCase
+
+@end
+
+@implementation Functional_ModuleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
